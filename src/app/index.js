1b343df9c2fba0f958cdad68a06dc51d709c1671
shahmir811 <shahmirkj@gmail.com>
2017-06-11 12:04:22 +0500

    Multiple Components

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import { render } from 'react-dom';
-//import ReactDOM from 'react-dom';
+//import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
 
 class App extends Component {
   render() {
@@ -8,13 +10,20 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <h1>Welcome Home</h1>
+            <Header />
           </div>
         </div>
+
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home />
+          </div>
+        </div>
+
       </div>
     );
   }
 }
 
 
-render(<App />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
